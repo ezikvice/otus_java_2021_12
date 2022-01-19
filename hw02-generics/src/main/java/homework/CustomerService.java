59275dd83990f5dd6e46d573059b85efceed34d0
Dmitry <shamparov@gmail.com>
2022-01-20 01:08:33 +0300

    hw02: сделан Customer, CustomerReverseOrder, начат CustomerService (пока не работает)

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,33 @@
 package homework;
 
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
+    Map<Customer, String> customersData = new NavigableMap<>(new Comparator<Customer>() {
+        @Override
+        public int compare(Customer o1, Customer o2) {
+            return ;
+        }
+    });
+
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
+
         return null; // это "заглушка, чтобы скомилировать"
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+//        return customersData.; // это "заглушка, чтобы скомилировать"
     }
 
     public void add(Customer customer, String data) {
-
+        customersData.put(customer, data);
     }
 }
