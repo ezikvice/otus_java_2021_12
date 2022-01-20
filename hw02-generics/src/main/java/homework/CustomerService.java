7db21b5bc7188c2f6438a50c22ab01d020a3bab4
Dmitry <shamparov@gmail.com>
2022-01-20 23:39:56 +0300

    hw02: CustomerService, тесты проходит. выглядит не очень

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,6 +1,8 @@
 package homework;
 
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,14 +10,25 @@ public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    TreeMap<Customer, String> customers = new TreeMap<>((o1, o2) -> Long.compare(o1.getScores(), o2.getScores()));
+    TreeMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        return customers.firstEntry();
+        Map.Entry<Customer, String> entryForCopy = customers.firstEntry();
+        Customer c = entryForCopy.getKey();
+        String data = entryForCopy.getValue();
+        Customer customer = new Customer(c.getId(), c.getName(), c.getScores());
+        return new AbstractMap.SimpleEntry<>(customer, data);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return customers.ceilingEntry(customer);
+        Map.Entry<Customer, String> entryForCopy = customers.higherEntry(customer);
+        if(entryForCopy == null) {
+            return null;
+        }
+        Customer c = entryForCopy.getKey();
+        String data = entryForCopy.getValue();
+        Customer copiedCustomer = new Customer(c.getId(), c.getName(), c.getScores());
+        return new AbstractMap.SimpleEntry<>(copiedCustomer, data);
     }
 
     public void add(Customer customer, String data) {
