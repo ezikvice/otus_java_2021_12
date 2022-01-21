3a0e0a048bdc6b1ac6ba992f3d73c757e4c3d5ef
Dmitry <shamparov@gmail.com>
2022-01-21 17:55:23 +0300

    hw02: небольшой рефакторинг

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -14,14 +14,21 @@ public class CustomerService {
 
     public Map.Entry<Customer, String> getSmallest() {
         Map.Entry<Customer, String> entryForCopy = customers.firstEntry();
-        Customer c = entryForCopy.getKey();
-        String data = entryForCopy.getValue();
-        Customer customer = new Customer(c.getId(), c.getName(), c.getScores());
-        return new AbstractMap.SimpleEntry<>(customer, data);
+        return getCopy(entryForCopy);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
         Map.Entry<Customer, String> entryForCopy = customers.higherEntry(customer);
+        return getCopy(entryForCopy);
+    }
+
+    public void add(Customer customer, String data) {
+        customers.put(customer, data);
+    }
+
+    //TODO: скорее всего есть более правильный способ вернуть глубокую копию Entry
+    // либо нужно использовать не TreeMap, а более специализированный класс
+    private Map.Entry<Customer, String> getCopy(Map.Entry<Customer, String> entryForCopy) {
         if(entryForCopy == null) {
             return null;
         }
@@ -29,9 +36,5 @@ public class CustomerService {
         String data = entryForCopy.getValue();
         Customer copiedCustomer = new Customer(c.getId(), c.getName(), c.getScores());
         return new AbstractMap.SimpleEntry<>(copiedCustomer, data);
-    }
-
-    public void add(Customer customer, String data) {
-        customers.put(customer, data);
     }
 }
