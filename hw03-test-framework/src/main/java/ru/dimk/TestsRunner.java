25dadf6fbc58fa782cfaffffcddd77ef96dccf58
Dimk <shamparov@gmail.com>
2022-01-30 15:05:12 +0300

    hw03: experiments with testRunner

--- hw03-test-framework/src/main/java/ru/dimk/TestsRunner.java
+++ hw03-test-framework/src/main/java/ru/dimk/TestsRunner.java
@@ -1,7 +1,6 @@
 package ru.dimk;
 
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,29 +8,49 @@ import java.util.List;
 
 public class TestsRunner {
 
+    private static List<Method> getAnnotatedMethods(Method[] methods, String annotationCanonicalName) {
+        List<Method> annotatedMethods = new ArrayList<>();
+        for (Method method : methods) {
+            if (Arrays.stream(method.getAnnotations()).anyMatch(
+                    annotation -> annotation.annotationType().getCanonicalName().equals(annotationCanonicalName))) {
+                annotatedMethods.add(method);
+            }
+        }
+        return annotatedMethods;
+    }
 
 
-
-    public static void testClass(String className){
+    public static void testClass(String className) {
         try {
             System.out.println("testing...");
             Class<?> c = Class.forName(className);
             Method[] methods = c.getMethods();
-            List<Method> beforeMethods = new ArrayList<>();
-            List<Method> afterMethods = new ArrayList<>();
-            for (Method method : methods) {
-                if(Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation.annotationType().getCanonicalName()=="ru.dimk.annotations.Before")) {
-                    beforeMethods.add(method);
-                }
-                else if (Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation.annotationType().getCanonicalName()=="ru.dimk.annotations.Before")) {
-                    afterMethods.add(method);
-                }
+            List<Method> beforeMethods = getAnnotatedMethods(methods, "ru.dimk.annotations.Before");
+            List<Method> afterMethods = getAnnotatedMethods(methods, "ru.dimk.annotations.After");
+            List<Method> testMethods = getAnnotatedMethods(methods, "ru.dimk.annotations.Test");
+//                for (Method method : methods) {
+//                    if (Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation.annotationType().getCanonicalName() == "ru.dimk.annotations.Before")) {
+//                        beforeMethods.add(method);
+//                    } else if (Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation.annotationType().getCanonicalName() == "ru.dimk.annotations.After")) {
+//                        afterMethods.add(method);
+//                    } else if (Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation.annotationType().getCanonicalName() == "ru.dimk.annotations.Test")) {
+//                        testMethods.add(method);
+//                    }
+//                }
+            System.out.println("Before methods: ");
+            for (Method method : beforeMethods) {
+                System.out.println(method.getName());
+                try {
 
-                System.out.println(method.getName() + ":" + method.getAnnotations());
-                for (Annotation annotation : method.getAnnotations()) {
-                    System.out.println(annotation.annotationType().getCanonicalName());
+                } catch (Exception e) {
+
                 }
             }
+            System.out.println("Test methods: ");
+            for (Method method : testMethods) {
+                System.out.println(method.getName());
+            }
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -39,11 +58,8 @@ public class TestsRunner {
 
     public static void main(String[] args) {
         testClass("ru.dimk.tests.AnnotationsTest");
-//        Class<?> clazz = AnnotationsTest.class;
-//        System.out.println(clazz.getCanonicalName());
 
     }
-
 
 
 }
