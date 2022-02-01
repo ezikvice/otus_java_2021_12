42ba9d446865f83bf1b85e71ee0dfc449f9ee305
Dmitrii <shamparov@gmail.com>
2022-02-01 09:39:18 +0300

    hw03: JUnit example

---
+++ hw03-test-framework/src/test/java/ru/dimk/test/AnnotationsTest.java
@@ -0,0 +1,46 @@
+package ru.dimk.test;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AnnotationsTest {
+
+    @BeforeEach
+    public void init() {
+        System.out.println("before1 ok");
+    }
+
+    @BeforeEach
+    public void init2() {
+        System.out.println("before2 ok");
+    }
+
+    @AfterEach
+    public void finish1() {
+        System.out.println("after1 ok");
+    }
+
+    @AfterEach
+    public void finish2() {
+        System.out.println("after2 ok");
+    }
+
+    @Test
+    public void run1GoodTest() {
+        System.out.println("good1 test ok");
+    }
+
+    @Test
+    public void run2GoodTest() {
+        System.out.println("good2 test ok");
+    }
+
+    @Test
+    public void run3RuntimeExceptionTest() {
+        throw new RuntimeException("oops.. RunTimeException raised");
+    }
+
+
+}
