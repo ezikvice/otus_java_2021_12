4cab8a9533a6b05944c04d715953ce4e2b024944
Dmitry <shamparov@gmail.com>
2022-04-28 12:49:08 +0300

    hw09: @Id перенесен в core

--- hw09-jdbc/demo/src/main/java/ru/otus/core/annotations/Id.java
+++ hw09-jdbc/demo/src/main/java/ru/otus/core/annotations/Id.java
@@ -1,4 +1,4 @@
-package ru.otus.annotations;
+package ru.otus.core.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,8 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+/*
+  Показывает, что данное поле является первичным ключом
+ */
 public @interface Id {
 }
