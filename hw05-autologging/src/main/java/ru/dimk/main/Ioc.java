122e83ab3c45ac6cacb9b65b26aa40f02fd487f1
Dmitry <shamparov@gmail.com>
2022-05-05 15:38:37 +0300

    hw05: рефакторинг

--- hw05-autologging/src/main/java/ru/dimk/main/Ioc.java
+++ hw05-autologging/src/main/java/ru/dimk/main/Ioc.java
@@ -29,13 +29,8 @@ class Ioc {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Method[] declaredMethods = this.proxedClass.getClass().getDeclaredMethods();
-
-            Optional<Method> methodFounded = Arrays.stream(declaredMethods)
-                    .filter(method1 -> methodsAreEquals(method1, method)).findFirst();
-            Method declaredMethod = methodFounded.orElse(null);
-
-            if (declaredMethod != null && declaredMethod.isAnnotationPresent(Log.class)) {
+            Method myMethod = proxedClass.getClass().getMethod(method.getName(), method.getParameterTypes());
+            if (myMethod.isAnnotationPresent(Log.class)) {
                 final String parametersString = Arrays.stream(args)
                         .map(Object::toString)
                         .collect(Collectors.joining(", ", "", ""));
@@ -44,24 +39,6 @@ class Ioc {
             }
 
             return method.invoke(proxedClass, args);
-        }
-
-        private boolean methodsAreEquals(Method first, Method second) {
-            return first.getName().equals(second.getName())
-                    && equalParamTypes(first.getParameterTypes(), second.getParameterTypes());
-        }
-
-        // стащил из java.base.java.lang.reflect.Executable.java:70
-        private boolean equalParamTypes(Class<?>[] params1, Class<?>[] params2) {
-            /* Avoid unnecessary cloning */
-            if (params1.length == params2.length) {
-                for (int i = 0; i < params1.length; i++) {
-                    if (params1[i] != params2[i])
-                        return false;
-                }
-                return true;
-            }
-            return false;
         }
 
         @Override
