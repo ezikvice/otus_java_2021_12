2c30fc0907dff8b090c62da01db79cc893f7dc6f
Dmitry <shamparov@gmail.com>
2022-05-23 15:54:16 +0300

    hw12: добавлены версии в gradle, добавлен веб сервер, авторизация через MyWebServerWithBasicSecurity, список всех клиентов

---
+++ hw12-web-server/src/main/java/ru/otus/WebServerWithBasicSecurityDemo.java
@@ -0,0 +1,75 @@
+package ru.otus;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.core.repository.DataTemplateHibernate;
+import ru.otus.core.repository.HibernateUtils;
+import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
+import ru.otus.crm.model.Address;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DBServiceClient;
+import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.helpers.FileSystemHelper;
+import ru.otus.server.MyWebServer;
+import ru.otus.server.MyWebServerWithBasicSecurity;
+import ru.otus.services.TemplateProcessor;
+import ru.otus.services.TemplateProcessorImpl;
+
+/*
+    Полезные для демо ссылки
+
+    // Стартовая страница
+    http://localhost:8080
+
+    // Страница пользователей
+    http://localhost:8080/clients
+
+    // REST сервис
+    http://localhost:8080/api/clients/3
+*/
+public class WebServerWithBasicSecurityDemo {
+    private static final int WEB_SERVER_PORT = 8080;
+    private static final String TEMPLATES_DIR = "/templates/";
+    private static final String HASH_LOGIN_SERVICE_CONFIG_NAME = "realm.properties";
+    private static final String REALM_NAME = "AnyRealm";
+
+
+    private static final Logger log = LoggerFactory.getLogger(WebServerWithBasicSecurityDemo.class);
+    public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+
+
+    public static void main(String[] args) throws Exception {
+        var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
+
+        var dbUrl = configuration.getProperty("hibernate.connection.url");
+        var dbUserName = configuration.getProperty("hibernate.connection.username");
+        var dbPassword = configuration.getProperty("hibernate.connection.password");
+
+        new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
+        var sessionFactory = HibernateUtils.buildSessionFactory(configuration,
+                Client.class, Address.class, Phone.class);
+        var transactionManager = new TransactionManagerHibernate(sessionFactory);
+
+        var clientTemplate = new DataTemplateHibernate<>(Client.class);
+        DBServiceClient dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
+
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
+
+        String hashLoginServiceConfigPath = FileSystemHelper.localFileNameOrResourceNameToFullPath(HASH_LOGIN_SERVICE_CONFIG_NAME);
+        LoginService loginService = new HashLoginService(REALM_NAME, hashLoginServiceConfigPath);
+
+        MyWebServer clientsWebServer = new MyWebServerWithBasicSecurity(WEB_SERVER_PORT,
+                loginService, dbServiceClient, gson, templateProcessor);
+
+        clientsWebServer.start();
+        clientsWebServer.join();
+    }
+}
