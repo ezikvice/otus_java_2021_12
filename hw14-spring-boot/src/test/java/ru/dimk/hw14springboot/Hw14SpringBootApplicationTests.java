7c97ea43e08127876fbca5b114d23ed755d2f477
Dmitry <shamparov@gmail.com>
2022-06-04 22:01:06 +0300

    hw14: first commit. Spring initializr

---
+++ hw14-spring-boot/src/test/java/ru/dimk/hw14springboot/Hw14SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dimk.hw14springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw14SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
