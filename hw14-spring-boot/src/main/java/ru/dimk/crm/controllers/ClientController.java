b98ff1bd2371f7246321d745312e11a6591b5e5e
dmitrii <d.shamparov@s-e.su>
2022-06-22 12:15:25 +0300

    hw_14: в основном логирование

--- hw14-spring-boot/src/main/java/ru/dimk/crm/controllers/ClientController.java
+++ hw14-spring-boot/src/main/java/ru/dimk/crm/controllers/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 import ru.dimk.crm.model.Client;
 import ru.dimk.crm.services.ClientService;
 
@@ -14,9 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api")
 public class ClientController {
-    @Autowired
+
     ClientService clientService;
 
     public ClientController(ClientService clientService) {
@@ -28,6 +28,18 @@ public class ClientController {
         List<Client> clients = clientService.findAll();
         model.addAttribute("clients", clients);
         return "clientsList";
+    }
+
+    @GetMapping("/client/create")
+    public String clientCreateView(Model model) {
+        model.addAttribute("client", new Client());
+        return "clientCreate";
+    }
+
+    @PostMapping("/client/save")
+    public RedirectView clientSave(@ModelAttribute Client client) {
+        clientService.save(client);
+        return new RedirectView("/", true);
     }
 
     @GetMapping("/clients")
--- hw14-spring-boot/src/main/java/ru/dimk/hw14springboot/WebConfig.java
+++ hw14-spring-boot/src/main/java/ru/dimk/hw14springboot/WebConfig.java
@@ -1,7 +1,8 @@
 package ru.dimk.hw14springboot;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/no-handler-view").setViewName("noHandlerView");
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 
 }
