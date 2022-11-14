924e520fd9524afd709229aa03a628eb64e5d6b0
Dmitrii <shamparov@gmail.com>
2022-11-14 16:05:44 +0200

    hw16: implementation of SensorDataProcessorBuffered thanks to https://github.com/AidanMegabyte for PriorityBlockingQueue.drainTo() method and Comparator.comparing() method

--- hw16-queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -2,9 +2,15 @@ package ru.otus.services.processors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 // Этот класс нужно реализовать
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
@@ -12,24 +18,29 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private BlockingQueue<SensorData> dataBuffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
+        dataBuffer.add(data);
         if (dataBuffer.size() >= bufferSize) {
             flush();
         }
-    */
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            List<SensorData> bufferedData = new ArrayList<>();
+            dataBuffer.drainTo(bufferedData);
+            if (!bufferedData.isEmpty()) {
+                writer.writeBufferedData(bufferedData);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
